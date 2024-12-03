dcaa42f5f2aa22d80ebe1243fce1f1c1e6dbb346
pgodettejr <109110310+pgodettejr@users.noreply.github.com>
2024-12-03 17:44:34 -0500

    Modify forecastResults function in app logic
    Added in several pieces of code as comments trying to get the correct
    approach toward what the forecastResults function needs to do via the
    pseudocode comments written just above it. The function is still not
    fully fleshed out and will need to be refactored. Potentially need a
    different approach than the 'for' loop currently there as right now
    I'm not sure how to get the object the for loop returns on each cycle
    into a new array that only contains the current temperature of a given
    location along with the 7 day forecast.

--- src/weatherAppLogic.js
+++ src/weatherAppLogic.js
@@ -20,12 +20,88 @@ async function getForecast(location) {
   // SET a variable equal to the "days" array 
   // INIT a "for" loop for the "days" array (represents 7 Day Forecast)
     // INIT the counter variable for this loop and set it's value to 1 (1 representing tomorrow's forecast)
-    // WHILE the counter is smaller than 7, increment the counter by 1
+    // WHILE the counter is smaller than 7, get the temp high, the temp low & the current temp of that day. Increment the counter by 1
     // REPEAT getting temperature and weather conditions for each future date after today's date at the location
     // UNTIL the 7th day after today's date is reached (include the 7th day's future forecast)
   // PRINT a new object that only has the current daily temp, the 7 day forecast and whether or not the temp should be F or C from the JSON
+  // PRINT = console.log()
 // END
 
-// TODO: Write a function that fetches the GIF that matches the day's forecast (linked to getForecast fetching the Weather Crossing API?)
+async function forecastResults() {
+  let weather = await getForecast();
+
+  let todaysTemp = weather.response.currentConditions.temp;
+  let futureTemps = weather.response.days;
+
+  // OPTION: 'Filter' method on 'futureTemps' to only get the first 7 items
+  for (let i = 1; i <= 7; i++) {
+    if (i <= 7) {
+      // May need to use 'map' method and/or 'filter' method and create a new array with those instead of what we have below currently
+      let futureDate = futureTemps[i].datetime;
+      let tempHigh = futureTemps[i].tempmax;
+      let tempLow = futureTemps[i].tempmin;
+
+      const outlook = {
+        futureDate,
+        tempHigh,
+        tempLow
+      };
+
+      // Possibly try 'Array.from' method to change each object to an array and do something with those new arrays?
+      // let arr = Array.from(outlook);
+      // alert(arr);
+      
+      return outlook; // May need this inside { } for closure reasons?
+    }
+  }
+
+  // let newObject = {
+  //   todaysTemp,
+  //   forecastResults.outlook
+  // };
+
+  // Possible 'for' loop alternative
+  // for (let i = 1; i < futureTemps.length; i++) {
+  //   if (futureTemps[i] > 7) {
+  //     break;
+  //   }
+
+  //   // May need to use 'map' method and/or 'filter' method and create a new array with those instead of what we have below currently
+  //   let futureDate = futureTemps[i].datetime;
+  //   let tempHigh = futureTemps[i].tempmax;
+  //   let tempLow = futureTemps[i].tempmin;
+
+  //   const outlook = {
+  //     futureDate,
+  //     tempHigh,
+  //     tempLow
+  //   };
+
+  //   return outlook; // May need this inside { } for closure reasons?
+  // }
+
+  // Maybe we CAN use a 'for...of' loop but get it to stop at the 7th element/index. My first attempt but 'day' (variable) wasn't defined
+  // for (const day of futureTemps) {
+  //   if (futureTemps[day] > 7) {
+  //     break;
+  //   }
+
+  //   let futureDate = futureTemps[day].datetime;
+  //   let tempHigh = futureTemps[day].tempmax;
+  //   let tempLow = futureTemps[day].tempmin;
+
+  //   const outlook = {
+  //     futureDate,
+  //     tempHigh,
+  //     tempLow
+  //   };
+    
+  //   return outlook;
+  // }
+
+  // OPTION: Using 'map' or 'filter' on the original 'days' array is another approach
+};
+
+// TODO: Write a function that fetches the GIF that matches the day's forecast (linked to getForecast fetching the Weather Crossing API?). We will need to utilize 'getForecast.response.currentConditions.conditions' to read the weather description.
 
 export { getForecast }
