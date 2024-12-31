cc0cf51012cda3f8230b015ef9a8e24015a34a02
pgodettejr <109110310+pgodettejr@users.noreply.github.com>
2024-12-31 11:35:29 -0500

    Modify comments and clean up some code
    Changed the TODO comments in the getWeatherGIF function related to the
    background GIF feature into BRANCH comments instead to reflect that
    they will be worked on after the MVP release of the app.
    
    Removed extra indentation in the renderCurrentTemp function that was
    related to a previous conditional 'if' statement in the function that
    has since been removed. Concatenated the Fahrenheit info to the
    temperature expression line as the temperature was not displaying in
    Fahrenheit or Celsius. It only displays the info given to us by the API
    which is just a Fahrenheit number (likely based on location of user's
    device where the input is searched from).

--- src/weatherUI.js
+++ src/weatherUI.js
@@ -8,78 +8,65 @@ const forecastDisplay = document.querySelector('.search-results');
 
 // let searchText = document.getElementById('search-text').value.trim();
 
-// Template for rendering the current/daily forecast
-// TODO: We need to take the 'outlook' object we get from 'processData' and render that info to the UI, not what we currently have now. Seeing if there's text in the search bar, then trying to grab the temp from the 'outlook' object for innerText afterwards doesn't make sense.
+// Template for rendering the current forecast
 async function renderCurrentTemp() {
   // let searchText = document.getElementById('search-text').value.trim();
 
   try {
     // CALL 'processData' function (or 'searchFilter' function?)
-      const fetchData = await processData();
+    const fetchData = await processData();
 
     // OBTAIN the 'outlook' object returned from the 'processData' (or 'searchFilter') function
     // SET a variable equal to the 'outlook' object returned from 'processData' function
-      const weatherInfo = fetchData.outlook;
+    const weatherInfo = fetchData.outlook; 
 
-    // *ATTEMPT: removed the 'if' statement entirely for testing as it doesn't make sense for this code to run based on search box text. It should just run either based on the object returned from 'processData' or based on its await call using the 'userLocation' as a parameter in the event listener (see 'userLocation' and 'locationTemp' variables in the event listener under the index module)
-    // if (searchText) { 
+    // CALL 'replaceChildren' method on the DOM element responsible for holding all UI display elements (forecastDisplay) to remove any weather data from previous searches
+    // OPTION: May not keep this [here] - move to search button event listener in index module?
+    forecastDisplay.replaceChildren(); 
 
-      // CALL 'replaceChildren' method on the DOM element responsible for holding all UI display elements (forecastDisplay) to remove any weather data from previous searches
-      // OPTION: May not keep this [here] - move to search button event listener in index module?
-      forecastDisplay.replaceChildren(); 
+    // CREATE a wrapper element to be added to the weather results area to contain the text used for displaying the weather
+    const weatherWrapper = document.createElement('div');
+    weatherWrapper.classList.add('weather-wrapper');
 
-      // CREATE a wrapper element to be added to the weather results area to contain the text used for displaying the weather
-      const weatherWrapper = document.createElement('div');
-      weatherWrapper.classList.add('weather-wrapper');
+    // CREATE a text element to hold the text that displays the actual temperature
+    let weatherTemp = document.createElement('h4');
+    weatherTemp.classList.add('weather-temp');
 
-      // CREATE a text element to hold the text that displays the actual temperature
-      let weatherTemp = document.createElement('h4');
-      weatherTemp.classList.add('weather-temp');
+    // CALL the 'innerText' method 
+    // SET the method equal to the 'temperature' key in the 'outlook' object in order to display the temperature
+    weatherTemp.innerText = weatherInfo.temperature + "°F"; 
 
-      // CALL the 'innerText' method and SET it equal to the 'temperature' key in the 'outlook' object in order to display the temperature
+    // CREATE a text element to display the weather condition equal to the 'icon set parameter'
+    const weatherCondition = document.createElement('p');
+    weatherCondition.classList.add('weather-condition');
+    weatherCondition.innerText = weatherInfo.summary;
 
-      // TODO: Test this line of code. If the text isn't correct, figure out how to get the temp results from the fetch request to show here
-      // Put this under button logic in index module if necessary
+    // CREATE a wrapper to contain both the location icon and the text for the actual location. Place the next 2 elements under it.
+    const locationWrapper = document.createElement('div');
+    locationWrapper.classList.add('location-wrapper');
 
-      // WAS: Uncaught TypeError: Cannot read properties of undefined (reading 'currentConditions')
-      // ATTEMPT #1: Removed '.response' from the expression 'getForecast.response.currentConditions.temp'
-      // ATTEMPT #2: Added async/await to the function & this line of code (await getForecast.currentConditions.temp)
-      // ATTEMPT #3: Changed to "processData.outlook.temperature"
-      // NOW: Uncaught TypeError: Cannot read properties of undefined (reading 'temperature')
-      weatherTemp.innerText = weatherInfo.temperature; 
+    // CREATE an image element for displaying the 'location' icon used in Google Maps
+    // TODO: this is a sample. Need to change 'Update' image link above after getting location/map icon from the internet (see Excalidraw)
+    const weatherIcon = new Image();
+    weatherIcon.src = Update;
+    weatherIcon.classList.add("image-button");
 
-      // CREATE a text element to display the weather condition equal to the 'icon set parameter'
-      const weatherCondition = document.createElement('p');
-      weatherCondition.classList.add('weather-condition');
-      weatherCondition.innerText = weatherInfo.summary;
+    // CREATE a text element to hold the text that displays the location searched for
+    let weatherLocation = document.createElement('p');
+    weatherLocation.classList.add('weather-location');
+    weatherLocation.innerText = weatherInfo.location;
+    
+    // APPEND the location elements under their related location wrapper
+    locationWrapper.appendChild(weatherIcon);
+    locationWrapper.appendChild(weatherLocation);
 
-      // CREATE a wrapper to contain both the location icon and the text for the actual location. Place the next 2 elements under it.
-      const locationWrapper = document.createElement('div');
-      locationWrapper.classList.add('location-wrapper');
+    // APPEND the location wrapper containing all location info, the temperature and the weather condition to the wrapper element
+    weatherWrapper.appendChild(weatherTemp);
+    weatherWrapper.appendChild(weatherCondition);
+    weatherWrapper.appendChild(locationWrapper);
 
-      // CREATE an image element for displaying the 'location' icon used in Google Maps
-      // TODO: this is a sample. Need to change 'Update' image link above after getting location/map icon from the internet (see Excalidraw)
-      const weatherIcon = new Image();
-      weatherIcon.src = Update;
-      weatherIcon.classList.add("image-button");
-
-      // CREATE a text element to hold the text that displays the location searched for
-      let weatherLocation = document.createElement('p');
-      weatherLocation.classList.add('weather-location');
-      weatherLocation.innerText = weatherInfo.location;
-      
-      // APPEND the location elements under their related location wrapper
-      locationWrapper.appendChild(weatherIcon);
-      locationWrapper.appendChild(weatherLocation);
-
-      // APPEND the location wrapper containing all location info, the temperature and the weather condition to the wrapper element
-      weatherWrapper.appendChild(weatherTemp);
-      weatherWrapper.appendChild(weatherCondition);
-      weatherWrapper.appendChild(locationWrapper);
-
-      // APPEND the wrapper element to the 'forecastDisplay' element (the UI container)
-      forecastDisplay.appendChild(weatherWrapper);
-    // }
+    // APPEND the wrapper element to the 'forecastDisplay' element (the UI container)
+    forecastDisplay.appendChild(weatherWrapper);
   } catch (err) {
     console.error(`Error: ${err}`);
   }
@@ -119,9 +106,9 @@ async function getWeatherGIF() {
         (weatherSummary === "snow") || (weatherSummary === "rain") || (weatherSummary === "fog") || 
         (weatherSummary === "wind") || (weatherSummary === "cloudy"): 
           // APPEND the GIF from the API to that <img> element as a background image
-          // SHOW the GIF as a background image for the weather results in the UI
+          // BRANCH: SHOW the GIF as a background image for the weather results in the UI
 
-          // OPTION: Potentially need a 'replaceChildren()' of some kind here if we change the <img> placeholder above
+          // OPTION: Potentially need a 'replaceChildren()' of some kind here if we change the <img> placeholder above for background image
           img.src = resultClip.data[0].images.original.url;
           img.style.backgroundImage = img.src;
           break; // OPTION: May need to be 'return true' instead
@@ -129,15 +116,15 @@ async function getWeatherGIF() {
         case // fetch() a random weather GIF from the GIPHY API for these 'icon2' conditions
         (weatherSummary.startsWith("partly-")) || (weatherSummary.startsWith("clear-")) || (weatherSummary.startsWith("thunder-")):
           // APPEND the GIF from the API to that <img> element as a background image
-          // SHOW the GIF as a background image for the weather results in the UI
+          // BRANCH: SHOW the GIF as a background image for the weather results in the UI
 
-          // OPTION: Potentially need a 'replaceChildren()' of some kind here if we change the <img> placeholder above
+          // OPTION: Potentially need a 'replaceChildren()' of some kind here if we change the <img> placeholder above for background image
           img.src = resultClip.data[0].images.original.url;
           img.style.backgroundImage = img.src;
           break; // OPTION: May need to be 'return true' instead
       }
 
-      // TODO: Move this conditional up above the switch statement if possible
+      // BRANCH: Move this conditional up above the switch statement if possible
       // IF there is no 'icon set parameter' OR there is no weather-related GIF that the GIPHY API can find
       if (!weatherSummary || !fetchClip) {
         // CREATE a <span> or <img> element showing some type of error message as text or an image (browser may show broken image as default)
@@ -148,7 +135,7 @@ async function getWeatherGIF() {
         summaryError.appendChild(errorMessage);
         forecastDisplay.appendChild(summaryError); // Placeholder for now. Will refactor this to show as a background image
 
-        // SHOW that element along with any error message in the UI
+        // BRANCH: SHOW that element along with any error message in the UI
         // TODO: Figure out how to display the error message as the background image itself (where the GIF would normally be). CSS 'background-image' property with 'url(link)' as the key? Create another container to use as the background and append the error to that?
 
         // 'return' nothing goes here?
